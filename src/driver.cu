5915768a9aef6c277ae966daa9f8a651bfda945d
Ashwin Gupta <ashwingupta2000@gmail.com>
2020-11-27 23:15:19 -0800

    added timer to main loop, worked on ransac

--- src/driver.cu
+++ src/driver.cu
@@ -5,6 +5,10 @@
 #include <iostream>
 #include "common.hpp"
 #include <Eigen/Dense>
+#include <chrono> 
+
+
+using namespace std::chrono; 
 
 /*
 Temporary driver program, do NOT copy this to mrover percep code at time of integration
@@ -29,14 +33,20 @@ int main(int argc, char** argv) {
     //This is a RANSAC model that we will use
     RansacPlane ransac(Eigen::Vector3d(0, 1, 0), 10, 400, 100, pcSize);
 
+    auto start = high_resolution_clock::now(); 
     while(viewer.isAvailable()) {
+        //loop timer
+   
         //grab the current point cloud
         zed.grab();
         zed.retrieveMeasure(gpu_cloud, sl::MEASURE::XYZRGBA, sl::MEM::GPU); 
         GPU_Cloud pc = getRawCloud(gpu_cloud);
         
+        auto ransacStart = high_resolution_clock::now();
         ransac.computeModel(pc);
-
+        auto ransacStop = high_resolution_clock::now();
+        auto ransacDuration = duration_cast<microseconds>(ransacStop - ransacStart); 
+        cout << "ransac time: " << (ransacDuration.count()/1.0e6) << endl; 
 
         //run a custom CUDA filter that will color the cloud blue
         //TestFilter test_filter(gpu_cloud);
@@ -47,7 +57,6 @@ int main(int argc, char** argv) {
         
         //update the viewer, the points will be blue
         viewer.updatePointCloud(gpu_cloud);
-        
     }
     gpu_cloud.free();
     zed.close();
--- src/plane-ransac.cu
+++ src/plane-ransac.cu
@@ -2,53 +2,84 @@
 #include "common.hpp"
 #include <stdlib.h>
 
-//These should definitely move to common.hpp
-__device__ float getX(GPU_Cloud pc, int index) {
+//These should definitely move to common.hpp but uncear on how to put device functions in header
+__device__ float getX(GPU_Cloud &pc, int index) {
     return pc.data[pc.stride * index + 0];
 }
 
-__device__ float getY(GPU_Cloud pc, int index) {
+__device__ float getY(GPU_Cloud &pc, int index) {
     return pc.data[pc.stride * index + 1];
 }
 
-__device__ float getZ(GPU_Cloud pc, int index) {
+__device__ float getZ(GPU_Cloud &pc, int index) {
     return pc.data[pc.stride * index + 2];
 }
 
-__device__ float3 getPoint(GPU_Cloud pc, int idx) {
-    
+__device__ float3 getPoint(GPU_Cloud &pc, int idx) {
     return make_float3(getX(pc, idx), getY(pc, idx), getZ(pc, idx));
 }
 
-__device__ float3 cross(float3 a, float3 b) { 
+__device__ float3 cross(float3 &a, float3 &b) { 
     return make_float3(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x);
 }
 
+__device__ float dot(float3 &a, float3 &b) { 
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+__device__ float norm(float3 &a) {
+    return sqrt(a.x*a.x + a.y*a.y + a.z*a.z);
+}
+
+__device__ float3 operator+(const float3 &a, const float3 &b) {
+    return make_float3(a.x+b.x, a.y+b.y, a.z+b.z);
+}
+
+__device__ float3 operator-(const float3 &a, const float3 &b) {
+    return make_float3(a.x-b.x, a.y-b.y, a.z-b.z);
+}
+
+__device__ float3 operator/(const float3 &a, const float b) {
+    return make_float3(a.x/b, a.y/b, a.z/b);
+}
+
+
+__device__ int ceilDivGPU(int a, int b) {
+    return (a + b - 1) / b;
+}
 
 /* 
+LAUNCH:
+    - [Block] # iterations [aka, randomly selected models] to try
+    - [Thread] MAX_THREADS
+
 REQUIRES:
-    - GPU data cloud
+    - GPU point cloud
     - A buffer to write inlier counts for each attempted model
-    - A buffer to write the randomly selected points for each attempted model
+    - A buffer that tells the kernel what the randomly selected points were for each model
+    - Threshold distance for a pt to be considered an inlier
 EFFECTS:
 
 Block: 
     Each block represents an "iteration" of the traditional RANSAC algorithm. 
-    That is, every block has a different set of randomly chosen 3 points to 
-    evaluate the model with. 
+    That is, every block has a different set of randomly chosen 3 points that define the
+    model (a plane is minimally defined by 3 points). The threads in the block then check
+    every point in the point cloud against the model for that block.
 Thread:
     Threads are used to decide how many points are inliers to the model. If
     thread max = 1024 and there are 2048 points, each thread will process 2
-    points. Each thread will right the number of inliers to its specific spot
-    in shared memory. Threads are synced, and then threads will participate
-    in a parallel reduction to give the total number of inliers, which will 
-    be returned from the kernel along with the three points that define the
-    model tested on this iteration.
-
+    points. Each thread will write the number of inliers in the set of points it evaluated
+    to its specific spot in shared memory. Threads are synced, and then threads will 
+    participate in a parallel reduction to give the total number of inliers for that block/model, 
+    which will be returned from the kernel in the inlierCounts buffer. 
 */
-__global__ void ransacKernel(GPU_Cloud pc, int* inlierCounts, int* modelPoints) {
+__global__ void ransacKernel(GPU_Cloud pc, int* inlierCounts, int* modelPoints, int threshold) {
+    __shared__ int inlierField[MAX_THREADS];
+    //inlierField[threadIdx.x] = 0;
+    int inliers = 0;
     int iteration = blockIdx.x;
 
+    // select 3 random points from the cloud as the model that this particular block will evaluate
     int randIdx0 = modelPoints[iteration*3 + 0];
     int randIdx1 = modelPoints[iteration*3 + 1];
     int randIdx2 = modelPoints[iteration*3 + 2];
@@ -56,22 +87,48 @@ __global__ void ransacKernel(GPU_Cloud pc, int* inlierCounts, int* modelPoints) 
     float3 modelPt1 = getPoint(pc, randIdx1);
     float3 modelPt2 = getPoint(pc, randIdx2);
 
-    int pointsPerThread = (pc.size + MAX_THREADS - 1) / MAX_THREADS;
+    // figure out how many points each thread must compute distance for and determine if each is inlier/outlier
+    int pointsPerThread = ceilDivGPU(pc.size, MAX_THREADS);
     for(int i = 0; i < pointsPerThread; i++) {
+        // select a point index or return if this isn't a valid point
         int pointIdx = threadIdx.x * pointsPerThread + i;
         if(pointIdx > pc.size) return; 
-
+        
+        // point in the point cloud that could be an inlier or outlier
         float3 curPt = getPoint(pc, pointIdx);
 
-        //calculate distance of cur pt to the plane formed by the 3 model points
+        // get the two vectors on the plane defined by the model points
+        float3 v1 = modelPt1 - modelPt0;
+        float3 v2 = modelPt2 - modelPt0;
+
+        //get a vector normal to the plane model
+        float3 n = cross(v1, v2);
+
+        //calculate distance of cur pt to the plane formed by the 3 model points [see doc for the complete derrivation]
+        float3 d_to_model_pt = (curPt - modelPt1);
+        float d = abs(dot(n, d_to_model_pt)) / norm(n);
 
         //add a 0 if inlier, 1 if not 
+        inliers += (d < threshold) ? 1 : 0; 
     }
     
-    //parallel reduction
+    //parallel reduction to get an aggregate sum of the number of inliers for this model
+    //this is all equivalent to sum(inlierField), but it does it in parallel
+    inlierField[threadIdx.x] = inliers;
+    __syncthreads();
+    int aliveThreads = (blockDim.x) / 2;
+	while (aliveThreads > 0) {
+		if (threadIdx.x < aliveThreads) {
+			inliers += inlierField[aliveThreads + threadIdx.x];
+			if (threadIdx.x >= (aliveThreads) / 2) inlierField[threadIdx.x] = inliers;
+		}
+		__syncthreads();
+		aliveThreads /= 2;
+	}
 
+    //at the final thread, write to global memory
     if(threadIdx.x == 0) {
-    //    inlierCounts[iteration] = 
+        inlierCounts[iteration] = inliers;
     }
 }
 
@@ -102,7 +159,7 @@ RansacPlane::Plane RansacPlane::computeModel(GPU_Cloud pc) {
 
     int blocks = iterations;
     int threads = MAX_THREADS;
-    ransacKernel<<<blocks, threads>>>(pc, inlierCounts, modelPoints);
+    ransacKernel<<<blocks, threads>>>(pc, inlierCounts, modelPoints, threshold);
     checkStatus(cudaGetLastError());
     checkStatus(cudaDeviceSynchronize());
 
